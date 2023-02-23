6ebb10845a511c95551a924c17dc51949f21d32b
Juan Priegue <118004400+JuanPriegue2003@users.noreply.github.com>
2023-02-23 13:51:53 -0300

    Create factorial-while.js

---
+++ factorial-while.js
@@ -0,0 +1,8 @@
+//factorial-while.js
+//Este archivo debe calcular el factorial de 10 utilizando un bucle while
+let totalB = 1;
+i = 1;
+while(i <= 10){
+    totalB = totalB*i;i++;
+}
+console.log(totalB);
