f2996a96ccc6dc53e4e718b2473ed30aba99b480
Juan Priegue <118004400+JuanPriegue2003@users.noreply.github.com>
2023-02-23 13:50:48 -0300

    Create factorial-for.js

---
+++ factorial-for.js
@@ -0,0 +1,6 @@
+//Factorial-for.js
+//Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+let total = 1; 
+for ( i=1; i<=10; i++) {
+total = total * i; 
+}     console.log(total);
